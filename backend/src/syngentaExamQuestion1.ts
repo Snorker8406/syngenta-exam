1c36382c76d417198e58e97b5d750df6cccad9c4
Jose Antonio A <snorker84@hotmail.com>
2025-01-25 19:13:17 -0600

    question2 response, adding typsecript and running with nodemon

---
+++ backend/src/syngentaExamQuestion1.ts
@@ -0,0 +1,44 @@
+import { PathLike } from 'fs';
+import { FileHandle, readFile } from 'fs/promises';
+
+
+export async function testWordsAnalisys(textFilePath: string) {
+  try {
+    const contentText = await readFile(textFilePath, 'utf-8');
+
+    const words = contentText.toLowerCase() // key insensitive
+      .replace(/[^a-z0-9\s]/g, '') //regex to remove special characters
+      .split(/\s+/); // regex to split by spaces
+
+    const wordsCount: Map<string, number> = new Map(); // Map to store the count of each word ['word', count]
+
+    for (const word of words) {
+      if (word) {
+        wordsCount.set(word, (Number(wordsCount.get(word)) || 0) + 1); // if word already exists, increment count
+      }
+    }
+    const wordsList = Array.from(wordsCount.entries());
+    return wordsList.sort((a, b) => b[1] - a[1]); // desc order by count
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(`error at processing text: ${err.message}`);
+    } else {
+      throw new Error('error at processing text: unknown error');
+    }
+  }
+}
+
+// Ejecución principal para probar la función
+(async () => {
+  try {
+    const result = await testWordsAnalisys('syngenta-exam/sampleText.txt');
+    console.log("word's list: ");
+    console.log(result);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('An unknown error occurred');
+    }
+  }
+})(); 
---
+++ backend/src/syngentaExamQuestion2.ts
@@ -0,0 +1,52 @@
+import { createReadStream } from 'fs';
+import { createInterface } from 'readline';
+
+/*
+* with this function we can count the words in a file using streams
+* this increase performance and reduce memory usage in case of a big file
+*/
+export async function testWordsAnalisys(path: string): Promise<[string, number][]> {
+  return new Promise((resolve, reject) => {
+    const wordsCount = new Map(); 
+
+    const fileStream = createReadStream(path, { encoding: 'utf-8' });
+    const rl = createInterface({
+      input: fileStream,
+      crlfDelay: Infinity, 
+    });
+
+    rl.on('line', (line) => { // process line by line instead of loading the whole file into memory
+        const words = line.toLowerCase()
+        .replace(/[^a-z0-9\s]/g, '')
+        .split(/\s+/);
+      for (const word of words) {
+        if (word) {
+          wordsCount.set(word, (wordsCount.get(word) || 0) + 1);
+        }
+      }
+    });
+
+    rl.on('close', () => {
+      resolve(Array.from(wordsCount.entries()).sort((a, b) => b[1] - a[1])); // desc order by count
+    });
+
+    rl.on('error', (err) => {
+      reject(new Error(`error at processing text: ${err.message}`));
+    });
+  });
+}
+
+
+(async () => {
+  try {
+    const result = await testWordsAnalisys('sampleText.txt');
+    console.log("word's list: ");
+    console.log(result);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('An unknown error occurred');
+    }
+  }
+})();
